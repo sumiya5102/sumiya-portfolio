e053781e83f5f6f6530b693f5d11f5e5353dc163
sumiya5102 <sunny.trading2909@gmail.com>
2025-07-02 22:12:54 -0700

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import './App.css';
-import Contact from './sections/Contact/Contact';
-import Footer from './sections/Footer/Footer';
-import Hero from './sections/Hero/Hero';
-import Projects from './sections/Projects/Projects';
-import Skills from './sections/Skills/Skills';
+import "./App.css";
+import Contact from "./sections/Contact/Contact";
+import Footer from "./sections/Footer/Footer";
+import Hero from "./sections/Hero/Hero";
+import Projects from "./sections/Projects/Projects";
+import Skills from "./sections/Skills/Skills";
 
 function App() {
   return (
     <>
       <Hero />
+      <video autoPlay loop muted playsInline className="background-video">
+        <source src="/bg.mp4" type="video/mp4" />
+      </video>
+
       <Projects />
       <Skills />
       <Contact />
